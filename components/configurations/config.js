48d621576186d7c099556671e8fb36b94948c16a
dglaze7 <rama_doni17@ymail.com>
2018-07-26 21:54:01 +0700

    prot config

--- components/configurations/config.js
+++ components/configurations/config.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-    port: 8000,
+    port: process.env.port || 3303,
     dbconn: 'mongodb://admin:admin123@ds253891.mlab.com:53891/xdatabase',
     dbname: 'xdatabase'
 }
